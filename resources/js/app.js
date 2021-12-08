1d447f5f9e25297e0207ee821897d745b5603465
DenysLeshchenko <denys.leshchenko@oa.edu.ua>
2021-12-08 22:29:59 +0200

    Vue, Vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,41 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 require('./bootstrap');
+import Vuetify from 'vuetify';
+window.Vue = require('vue').default;
+window.Vue.use(Vuetify);
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
 
-import Alpine from 'alpinejs';
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-window.Alpine = Alpine;
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('posts-table-component', require('./components/PostsTableComponent.vue').default);
+Vue.component('vuetify-table-component', require('./components/VuetifyTableComponent').default);
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
 
-Alpine.start();
+vuetify: new Vuetify();
+
+
+
+const app = new Vue({
+
+    vuetify: new Vuetify(),
+
+    el: '#app',
+
+});
